7d3df185687a5d4bbf20a5a75fa0eaf681c31759
Rafael Laverde <leafar91@gmail.com>
2017-05-09 11:42:55 -0500

    Skip QStandardPaths test in pyside2.

--- qtpy/tests/test_qdesktopservice_split.py
+++ qtpy/tests/test_qdesktopservice_split.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import
 
 import pytest
-from qtpy.QtCore import QStandardPaths
-from qtpy.QtGui import QDesktopServices
+from qtpy import PYSIDE2
 
 """Test QDesktopServices split in Qt5."""
 
+@pytest.mark.skipif(PYSIDE2, reason="It fails on PySide2")
 def test_qstandarpath():
     """Test the qtpy.QStandardPaths namespace"""
+    from qtpy.QtCore import QStandardPaths
 
     assert QStandardPaths.StandardLocation is not None
 
@@ -15,8 +16,10 @@ def test_qstandarpath():
     with pytest.raises(AttributeError) as excinfo:
         QStandardPaths.setUrlHandler
 
+@pytest.mark.skipif(PYSIDE2, reason="It fails on PySide2")
 def test_qdesktopservice():
     """Test the qtpy.QDesktopServices namespace"""
+    from qtpy.QtGui import QDesktopServices
 
     assert QDesktopServices.setUrlHandler is not None
 
