b98d0a8c29d95e8a2413be458b49d84589d0fc40
Carlos Cordoba <ccordoba12@gmail.com>
2018-04-28 13:50:20 -0500

    Run QDesktopServices tests in PySide2

--- qtpy/tests/test_qdesktopservice_split.py
+++ qtpy/tests/test_qdesktopservice_split.py
@@ -1,12 +1,12 @@
+"""Test QDesktopServices split in Qt5."""
+
 from __future__ import absolute_import
 
 import pytest
 import warnings
-from qtpy import PYQT4, PYSIDE, PYSIDE2
+from qtpy import PYQT4, PYSIDE
 
-"""Test QDesktopServices split in Qt5."""
 
-@pytest.mark.skipif(PYSIDE2, reason="It fails on PySide2")
 def test_qstandarpath():
     """Test the qtpy.QStandardPaths namespace"""
     from qtpy.QtCore import QStandardPaths
@@ -17,7 +17,7 @@ def test_qstandarpath():
     with pytest.raises(AttributeError) as excinfo:
         QStandardPaths.setUrlHandler
 
-@pytest.mark.skipif(PYSIDE2, reason="It fails on PySide2")
+
 def test_qdesktopservice():
     """Test the qtpy.QDesktopServices namespace"""
     from qtpy.QtGui import QDesktopServices
