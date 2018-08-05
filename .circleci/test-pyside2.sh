9226f287736054cc4842d7c6559d8ff32c15078f
Carlos Cordoba <ccordoba12@gmail.com>
2018-08-05 14:31:17 -0500

    Testing: Install PySide2 wheels in quiet mode

--- .circleci/test-pyside2.sh
+++ .circleci/test-pyside2.sh
@@ -9,13 +9,13 @@ export URL="http://download.qt.io/snapshots/ci/pyside/5.11/latest/"
 if [ "$USE_CONDA" = "Yes"]; then
     if [ "$PYTHON_VERSION" = "2.7"]; then
         conda remove -q qt pyqt
-        pip install --index-url=${URL} pyside2 --trusted-host download.qt.io
+        pip install -q --index-url=${URL} pyside2 --trusted-host download.qt.io
     else
         exit 0
     fi
 else
     pip uninstall -q -y pyqt5 sip
-    pip install --index-url=${URL} pyside2 --trusted-host download.qt.io
+    pip install -q --index-url=${URL} pyside2 --trusted-host download.qt.io
 fi
 
 python qtpy/tests/runtests.py
