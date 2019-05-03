75dba9be6d1d7e3541d91db2c470bb8ed1289cc2
Carlos Cordoba <ccordoba12@gmail.com>
2019-05-03 11:58:33 +0200

    Testing: Skip testing PyQt4 and PySide in Python 3.5

--- .circleci/test-pyside.sh
+++ .circleci/test-pyside.sh
@@ -5,6 +5,10 @@ conda activate test
 
 if [ "$USE_CONDA" = "No" ]; then
     exit 0
+elif [ "$PYTHON_VERSION" = "3.5" ]; then
+    # conda-forge doesn't provide packages for
+    # Python 3.5 anymore.
+    exit 0
 else
     conda remove -q qt pyqt
     conda install -q -c conda-forge qt=4.* pyside
