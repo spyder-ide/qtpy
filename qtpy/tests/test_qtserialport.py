42c56084ad96dd226db0abbda37bd702d7a37d25
Stanowczo <marcin@stanowczo.pl>
2020-02-27 10:16:06 +0100

    Added support for QtSerialPort add-on

---
+++ qtpy/tests/test_qtserialport.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+import pytest
+from qtpy import PYQT5
+
+@pytest.mark.skipif(not PYQT5, reason="Only available in Qt5 bindings, but still not in PySide2")
+def test_qtserialport():
+    """Test the qtpy.QtSerialPort namespace"""
+    from qtpy import QtSerialPort
+
+    assert QtSerialPort.QSerialPort is not None
+    assert QtSerialPort.QSerialPortInfo is not None
