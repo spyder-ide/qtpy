d2813779d8714936dc5450c37b228a2d1fcd94b3
phil65 <philipptemminghoff@googlemail.com>
2020-07-13 20:08:34 +0200

    add QtWinExtras module

---
+++ qtpy/tests/test_qtwinextras.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import sys
+
+import pytest
+from qtpy import PYQT5, PYSIDE2
+
+
+@pytest.mark.skipif(sys.platform != "win32" or not (PYQT5 or PYSIDE2),
+                    reason="Only available in Qt5 bindings and Windows platform")
+def test_qtwinextras():
+    """Test the qtpy.QtWinExtras namespace"""
+    from qtpy import QtWinExtras
+    assert QtWinExtras.QWinJumpList is not None
+    assert QtWinExtras.QWinJumpListCategory is not None
+    assert QtWinExtras.QWinJumpListItem is not None
+    assert QtWinExtras.QWinTaskbarButton is not None
+    assert QtWinExtras.QWinTaskbarProgress is not None
+    assert QtWinExtras.QWinThumbnailToolBar is not None
+    assert QtWinExtras.QWinThumbnailToolButton is not None
+    assert QtWinExtras.QtWin is not None
+
+    if PYSIDE2:
+        assert QtWinExtras.QWinColorizationChangeEvent is not None
+        assert QtWinExtras.QWinCompositionChangeEvent is not None
+        assert QtWinExtras.QWinEvent is not None
+
