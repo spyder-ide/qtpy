76a89ba89aa74f5c002e49a21c63e0434891e7c4
René Fritze <rene.fritze@wwu.de>
2022-01-12 10:29:30 +0100

    [qtopengl] harmonize QtGui pull-ins

--- qtpy/QtOpenGL.py
+++ qtpy/QtOpenGL.py
@@ -11,14 +11,24 @@ from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6, PythonQtError
 
 if PYQT5:
     from PyQt5.QtOpenGL import *
+    from PyQt5.QtGui import (QOpenGLBuffer, QOpenGLFramebufferObject, QOpenGLFramebufferObjectFormat, QOpenGLShader,
+                             QOpenGLShaderProgram, QOpenGLContext, QOpenGLContextGroup, QOpenGLDebugLogger,
+                             QOpenGLDebugMessage,QOpenGLPixelTransferOptions, QOpenGLTexture, QOpenGLTextureBlitter,
+                             QOpenGLTimeMonitor, QOpenGLTimerQuery, QOpenGLVersionProfile, QOpenGLVertexArrayObject,
+                             QOpenGLWindow)
 elif PYQT6:
     from PyQt6.QtOpenGL import *
+    from PySide6.QtGui import (QOpenGLContext, QOpenGLContextGroup)
 elif PYSIDE6:
     from PySide6.QtOpenGL import *
+    from PySide6.QtGui import (QOpenGLContext, QOpenGLContextGroup)
 elif PYSIDE2:
     from PySide2.QtOpenGL import *
-    from PySide2.QtGui import QOpenGLBuffer, QOpenGLFramebufferObject, QOpenGLFramebufferObjectFormat, QOpenGLShader, \
-        QOpenGLShaderProgram
+    from PySide2.QtGui import (QOpenGLBuffer, QOpenGLFramebufferObject, QOpenGLFramebufferObjectFormat, QOpenGLShader,
+                               QOpenGLShaderProgram, QOpenGLContext, QOpenGLContextGroup, QOpenGLDebugLogger,
+                               QOpenGLDebugMessage,QOpenGLPixelTransferOptions, QOpenGLTexture, QOpenGLTextureBlitter,
+                               QOpenGLTimeMonitor, QOpenGLTimerQuery, QOpenGLVersionProfile, QOpenGLVertexArrayObject,
+                               QOpenGLWindow)
 else:
     raise PythonQtError('No Qt bindings could be found')
 
