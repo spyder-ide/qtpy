3f0c4e22f30fe2e89444d7ffe38de30f84e42461
David Kincaid <daelonsuzuka@gmail.com>
2022-05-16 16:48:35 -0400

    Fix failing MacOS test

--- qtpy/tests/test_qtdbus.py
+++ qtpy/tests/test_qtdbus.py
@@ -3,7 +3,7 @@ import platform
 from qtpy import PYSIDE2, PYSIDE6, PYQT5, PYQT6
 
 @pytest.mark.skipif(PYSIDE2, reason="Not available in PySide2")
-@pytest.mark.skipif(platform.system() == 'Windows', reason="Not available on Windows")
+@pytest.mark.skipif(platform.system() != 'Linux', reason="Only available on Linux")
 def test_qtdbus():
     """Test the qtpy.QtDBus namespace"""
     from qtpy import QtDBus
