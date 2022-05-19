b41c5b8b55edc32be716d29d6aa76cd83b7a9003
David Kincaid <daelonsuzuka@gmail.com>
2022-05-19 09:29:34 -0400

    Fix QtDBus import conditions, hopefully

--- qtpy/QtDBus.py
+++ qtpy/QtDBus.py
@@ -8,18 +8,18 @@
 """Provides QtDBus classes and functions."""
 
 import sys
-from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6, PythonQtError, QtBindingMissingModuleError
+from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6, QtBindingsNotFoundError, QtBindingMissingModuleError, QtModuleNotFoundError
 
-if sys.platform == 'linux':
-    if PYQT5:
-        from PyQt5.QtDBus import *
-    elif PYQT6:
-        from PyQt6.QtDBus import *
-    elif PYSIDE2:
-        raise QtBindingMissingModuleError(name='QtDBus')
-    elif PYSIDE6:
+if PYQT5:
+    from PyQt5.QtDBus import *
+elif PYQT6:
+    from PyQt6.QtDBus import *
+elif PYSIDE2:
+    raise QtBindingMissingModuleError(name='QtDBus')
+elif PYSIDE6:
+    if sys.platform != 'win32':
         from PySide6.QtDBus import *
     else:
-        raise PythonQtError("No Qt bindings could be found")
+        raise QtModuleNotFoundError(name='QtDBus', msg='{name} does not exist on this operating system.')
 else:
-    raise PythonQtError('QtDBus does not exist on this operating system')
+    raise QtBindingsNotFoundError()
--- qtpy/tests/test_qtdbus.py
+++ qtpy/tests/test_qtdbus.py
@@ -2,11 +2,11 @@ import pytest
 import sys
 from qtpy import PYSIDE2, PYSIDE6, PYQT5, PYQT6
 
-@pytest.mark.skipif(PYSIDE2, reason="Not available in PySide2")
-@pytest.mark.skipif(sys.platform != 'linux', reason="Only available on Linux")
+# @pytest.mark.skipif(PYSIDE2, reason="Not available in PySide2")
+# @pytest.mark.skipif(sys.platform != 'linux', reason="Only available on Linux")
 def test_qtdbus():
     """Test the qtpy.QtDBus namespace"""
-    from qtpy import QtDBus
+    QtDBus = pytest.importorskip("qtpy.QtDBus")
 
     assert QtDBus.QDBusAbstractAdaptor is not None
     assert QtDBus.QDBusAbstractInterface is not None
