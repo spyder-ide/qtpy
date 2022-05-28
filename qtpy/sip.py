02df403d2e64ffd1f6d6249c2b5214a754c0df9d
David Kincaid <daelonsuzuka@gmail.com>
2022-05-28 14:28:20 -0400

    Standardize sip module

--- qtpy/sip.py
+++ qtpy/sip.py
@@ -1,15 +1,21 @@
-#
+# -----------------------------------------------------------------------------
 # Copyright © 2009- The Spyder Development Team
 #
 # Licensed under the terms of the MIT License
 # (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
 
-from . import PYQT6, PYQT5, PythonQtError
+"""Provides access to sip."""
 
-if PYQT6:
-    from PyQt6.sip import *
-elif PYQT5:
+from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6, PythonQtError, QtBindingMissingModuleError
+
+if PYQT5:
     from PyQt5.sip import *
+elif PYQT6:
+    from PyQt6.sip import *
+elif PYSIDE2:
+    raise QtBindingMissingModuleError(name='sip')
+elif PYSIDE6:
+    raise QtBindingMissingModuleError(name='sip')
 else:
-    raise PythonQtError(
-        'Currently selected Qt binding does not support this module')
+    raise PythonQtError('No Qt bindings could be found')
