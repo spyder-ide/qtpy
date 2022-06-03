8c37d9493fbe45012d361c03675fb0de4b726827
David Kincaid <daelonsuzuka@gmail.com>
2022-06-03 12:42:51 -0400

    Implement QtX11Extras test

--- qtpy/tests/test_qtx11extras.py
+++ qtpy/tests/test_qtx11extras.py
@@ -1,17 +1,8 @@
-import sys
 import pytest
 
-from qtpy import PYQT6, PYSIDE2, PYSIDE6
-from qtpy.tests.utils import using_conda
-
-# @pytest.mark.skipif(
-#     PYQT6 or PYSIDE6, reason="Not availible on Qt6-based bindings")
-# @pytest.mark.skipif(
-#     sys.platform != "win32" or using_conda(),
-#     reason="Only available in Qt5 bindings > 5.9 with pip on Windows in CIs")
 def test_qtwinextras():
     QtX11Extras = pytest.importorskip("qtpy.QtX11Extras")
 
-    # TODO: this is just a placeholder file
-
-    # assert QtX11Extras.QSomething is not None
+    assert QtX11Extras is not None
+    # This module doesn't seem to contain any classes
+    # See https://doc.qt.io/qt-5/qtx11extras-module.html
