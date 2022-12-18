1b44776bdd663ad0224360766188978c0bf1496f
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2022-12-18 17:25:44 -0600

    Fix a few minor issues with overhauled CI config
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -9,8 +9,13 @@ conda remove -q -n test-env --all || true
 # Create and activate conda environment for this test
 BINDING=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 QT_VERSION_VAR=${BINDING}_QT_VERSION
+
 # pytest-qt >=4 doesn't support Qt <=5.9
-if [ "${!QT_VERSION_VAR:0:3}" = "5.9" ]; then PYTESTQT_VERSION="=3.3.0"; PYTEST_VERSION=">=6,!=7.0.0,!=7.0.1,<7.2.0"; fi
+if [ "${!QT_VERSION_VAR:0:3}" = "5.9" ]; then
+    PYTESTQT_VERSION="=3.3.0"
+    PYTEST_VERSION=">=6,!=7.0.0,!=7.0.1,<7.2.0"
+fi
+
 conda create -q -n test-env python=${PYTHON_VERSION} pytest${PYTEST_VERSION:-">=6,!=7.0.0,!=7.0.1"} "pytest-cov>=3.0.0" pytest-qt${PYTESTQT_VERSION:-}
 conda activate test-env
 
