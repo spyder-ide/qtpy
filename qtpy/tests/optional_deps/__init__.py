4b921da758e57c94c2e6a37113658d0cdbb2fda5
David Kincaid <daelonsuzuka@gmail.com>
2023-04-04 13:28:13 -0400

    Add a test for the deferred import error mechanism

---
+++ qtpy/tests/optional_deps/__init__.py
@@ -0,0 +1,27 @@
+"""Module used for testing the deferred import error mechanism"""
+
+
+# See https://github.com/spyder-ide/qtpy/pull/387/
+
+
+from qtpy.utils import _getattr_missing_optional_dep
+from .optional_dep import ExampleClass
+
+
+_missing_optional_names = {}
+
+
+try:
+    from .optional_dep import MissingClass
+except ImportError as error:
+    _missing_optional_names['MissingClass'] = {
+        'name': 'optional_dep.MissingClass',
+        'missing_package': 'test_package_please_ignore',
+        'import_error': error,
+    }
+
+
+def __getattr__(name):
+    """Custom getattr to chain and wrap errors due to missing optional deps."""
+    raise _getattr_missing_optional_dep(
+        name, module_name=__name__, optional_names=_missing_optional_names)
---
+++ qtpy/tests/optional_deps/optional_dep.py
@@ -0,0 +1,4 @@
+
+
+class ExampleClass:
+    pass
