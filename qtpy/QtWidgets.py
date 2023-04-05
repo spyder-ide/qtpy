32e2f04b0a36c0211322d7b22cddfe1571fc2a21
stsav012 <stsav012@gmail.com>
2023-04-05 12:33:21 +0300

    Call static `QMenu.exec` when necessary

--- qtpy/QtWidgets.py
+++ qtpy/QtWidgets.py
@@ -10,6 +10,18 @@
 
 from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6
 
+
+def _possibly_static_exec(cls, *args, **kwargs):
+    """ Call `self.exec` when `self` is given or a static method otherwise. """
+    if isinstance(args[0], cls):
+        if len(args) == 1 and not kwargs:
+            # A special case to avoid the function resolving error
+            return args[0].exec()
+        return args[0].exec(*args[1:], **kwargs)
+    else:
+        return cls.exec(*args, **kwargs)
+
+
 if PYQT5:
     from PyQt5.QtWidgets import *
 elif PYQT6:
@@ -27,7 +39,7 @@ elif PYQT6:
     QPlainTextEdit.print_ = lambda self, *args, **kwargs: self.print(*args, **kwargs)
     QApplication.exec_ = QApplication.exec
     QDialog.exec_ = lambda self, *args, **kwargs: self.exec(*args, **kwargs)
-    QMenu.exec_ = lambda self, *args, **kwargs: self.exec(*args, **kwargs)
+    QMenu.exec_ = lambda *args, **kwargs: _possibly_static_exec(QMenu, *args, **kwargs)
     QLineEdit.getTextMargins = lambda self: (self.textMargins().left(), self.textMargins().top(), self.textMargins().right(), self.textMargins().bottom())
 
     # Allow unscoped access for enums inside the QtWidgets module
@@ -51,4 +63,4 @@ elif PYSIDE6:
     # Map DeprecationWarning methods
     QApplication.exec_ = QApplication.exec
     QDialog.exec_ = lambda self, *args, **kwargs: self.exec(*args, **kwargs)
-    QMenu.exec_ = lambda self, *args, **kwargs: self.exec(*args, **kwargs)
+    QMenu.exec_ = lambda *args, **kwargs: _possibly_static_exec(QMenu, *args, **kwargs)
